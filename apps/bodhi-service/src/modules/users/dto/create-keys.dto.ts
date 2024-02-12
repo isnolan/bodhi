26e0d195bf58c1a648097df0e377a6cc5117bdef
yhostc <yhostc@gmail.com>
2024-02-12 17:32:43 +0800

    chore: update

--- apps/bodhi-service/src/modules/users/dto/create-keys.dto.ts
+++ apps/bodhi-service/src/modules/users/dto/create-keys.dto.ts
@@ -1,20 +1,19 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsDate, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateKeysDto {
   @ApiPropertyOptional({ description: 'foreign id', example: '' })
-  @IsString()
   @IsNotEmpty()
+  @IsString()
   foreign_user_id: string;
 
-  @ApiPropertyOptional({ description: 'Quota', example: 0 })
-  @IsNumber()
-  quota: number;
-
-  @ApiPropertyOptional({ description: 'expire at', example: '' })
+  @ApiPropertyOptional({ description: 'expire at', example: new Date() })
+  @IsOptional()
+  @IsDateString()
   expires_at: Date;
 
   @ApiPropertyOptional({ description: 'note', example: '' })
+  @IsOptional()
   @IsString()
   note: string;
 }
