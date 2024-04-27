ac538c60394dcc42fd4e5585d4ace7e9014e2e45
yhostc <yhostc@gmail.com>
2024-04-27 23:18:27 +0800

    chore: update

--- packages/bodhi-adapter/test/aliyun-qwen.test.ts
+++ packages/bodhi-adapter/test/aliyun-qwen.test.ts
@@ -23,10 +23,10 @@ describe('chat', () => {
       messages: [
         // { role: 'system', content: 'You are a helpful assistant.' },
         // { role: 'user', content: '你好，哪个公园距离我最近？' },
-        // { role: 'user', parts: [{ type: 'text', text: 'Hello, 我们家有两只狗' }] },
-        // { role: 'assistant', parts: [{ type: 'text', text: 'Great to meet you. What would you like to know?' }] },
-        // { role: 'user', parts: [{ type: 'text', text: '请写一篇关于我家小狗子的故事，要求字数不少于200字' }] },
-        { role: 'user', parts: [{ type: 'text', text: '今天最新AI科技新闻是什么？' }] },
+        { role: 'user', parts: [{ type: 'text', text: 'Hello, 我们家有两只狗' }] },
+        { role: 'assistant', parts: [{ type: 'text', text: 'Great to meet you. What would you like to know?' }] },
+        { role: 'user', parts: [{ type: 'text', text: '请写一篇关于我家小狗子的故事，要求字数不少于200字' }] },
+        // { role: 'user', parts: [{ type: 'text', text: '今天最新AI科技新闻是什么？' }] },
       ],
       top_k: 1.0,
       // top_p
@@ -40,32 +40,32 @@ describe('chat', () => {
   }, 30000);
 
   // image, from url
-  // it('image: from url', async () => {
-  //   const res = await api.sendMessage({
-  //     model: 'qwen-plus',
-  //     messages: [
-  //       {
-  //         role: 'system',
-  //         parts: [{ type: 'text', text: 'You are a helpful assistant.' }],
-  //       },
-  //       {
-  //         role: 'user',
-  //         parts: [
-  //           {
-  //             type: 'image',
-  //             url: 'https://dashscope.oss-cn-beijing.aliyuncs.com/images/dog_and_girl.jpeg',
-  //           },
-  //           { type: 'text', text: 'Describe this image' },
-  //         ],
-  //       },
-  //     ],
-  //     onProgress: (choices) => {
-  //       console.log(`[qwen]muliti`, JSON.stringify(choices));
-  //       expect(choices).toBeInstanceOf(Object);
-  //     },
-  //   });
+  it('image: from url', async () => {
+    const res = await api.sendMessage({
+      model: 'qwen-plus',
+      messages: [
+        {
+          role: 'system',
+          parts: [{ type: 'text', text: 'You are a helpful assistant.' }],
+        },
+        {
+          role: 'user',
+          parts: [
+            {
+              type: 'image',
+              url: 'https://dashscope.oss-cn-beijing.aliyuncs.com/images/dog_and_girl.jpeg',
+            },
+            { type: 'text', text: 'Describe this image' },
+          ],
+        },
+      ],
+      onProgress: (choices) => {
+        console.log(`[qwen]muliti`, JSON.stringify(choices));
+        expect(choices).toBeInstanceOf(Object);
+      },
+    });
 
-  //   console.log(`[qwen]muliti`, JSON.stringify(res));
-  //   expect(res).toBeInstanceOf(Object);
-  // }, 30000);
+    console.log(`[qwen]muliti`, JSON.stringify(res));
+    expect(res).toBeInstanceOf(Object);
+  }, 30000);
 });
