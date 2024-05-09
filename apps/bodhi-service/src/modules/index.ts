12c941ae7dc2004538343771c2cd53f38ab2c6f1
yhostc <yhostc@gmail.com>
2024-05-09 12:38:03 +0800

    chore: disable gpts module

--- apps/bodhi-service/src/modules/index.ts
+++ apps/bodhi-service/src/modules/index.ts
@@ -2,7 +2,7 @@ import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { FilesModule } from './files/files.module';
 // import { ToolsModule } from './tools/tools.module';
-import { GptsModule } from './gpts/gpts.module';
+// import { GptsModule } from './gpts/gpts.module';
 import { ProviderModule } from './provider/provider.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { SupplierModule } from './supplier/supplier.module';
@@ -12,7 +12,7 @@ export default [
   ChatModule,
   FilesModule,
   // ToolsModule,
-  GptsModule,
+  // GptsModule,
   SupplierModule,
   ProviderModule,
   SubscriptionModule,
