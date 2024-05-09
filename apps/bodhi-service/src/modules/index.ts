c8c2aca07e3b08e72a30b7408dade9c54f38fdad
yhostc <yhostc@gmail.com>
2024-05-09 15:04:45 +0800

    chore: update

--- apps/bodhi-service/src/modules/index.ts
+++ apps/bodhi-service/src/modules/index.ts
@@ -1,19 +1,8 @@
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { FilesModule } from './files/files.module';
-// import { ToolsModule } from './tools/tools.module';
-// import { GptsModule } from './gpts/gpts.module';
 import { ProviderModule } from './provider/provider.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { SupplierModule } from './supplier/supplier.module';
 
-export default [
-  AuthModule,
-  ChatModule,
-  FilesModule,
-  // ToolsModule,
-  // GptsModule,
-  SupplierModule,
-  ProviderModule,
-  SubscriptionModule,
-];
+export default [AuthModule, ChatModule, FilesModule, SupplierModule, ProviderModule, SubscriptionModule];
