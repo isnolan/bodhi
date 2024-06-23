6aa49234670b8b53a498447b5fd211ab91d414db
yhostc <yhostc@gmail.com>
2024-06-23 08:58:46 +0800

    chore: remove subscription code

--- apps/bodhi-service/src/modules/subscription/dto/consume.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-
-export class SubscriptionConsumed {
-  @ApiPropertyOptional()
-  times?: number;
-
-  @ApiPropertyOptional()
-  tokens?: number;
-}
